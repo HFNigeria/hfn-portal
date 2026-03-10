3d3e638441f83f53150dd3c14dbbf72908063c63
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-03-10 15:34:13 +0100

    Update Resources.vue

--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -226,12 +226,31 @@
     <div class="mb-4">
       <label class="text-sm font-medium text-gray-700">Your Email</label>
       <input
-        v-model="buyerEmail"
+        v-model="form.buyerEmail"
         type="email"
         class="w-full border rounded-lg px-3 py-2 mt-1"
         placeholder="Enter email to receive document"
       />
     </div>
+    <div class="mb-4">
+      <label class="text-sm font-medium text-gray-700">Your Name</label>
+      <input
+        v-model="form.name"
+        type="name"
+        class="w-full border rounded-lg px-3 py-2 mt-1"
+        placeholder="Enter name"
+      />
+    </div>
+<div class="mb-4">
+      <label class="text-sm font-medium text-gray-700">Your Organization</label>
+      <input
+        v-model="form.organization"
+        type="organization"
+        class="w-full border rounded-lg px-3 py-2 mt-1"
+        placeholder="Enter Organization"
+      />
+    </div>
+
 
     <p class="text-xs text-gray-500 mb-4">
       Session expires in {{ timer }} seconds
@@ -287,7 +306,12 @@ import { ref } from "vue";
 const showPaymentDialog = ref(false);
 const showSuccessDialog = ref(false);
 
-const buyerEmail = ref("");
+const form = ref({
+  buyerEmail: "",
+  name: "",
+  organization: ""
+})
+
 const timer = ref(300); // 5 minutes
 
 let interval = null;
@@ -315,8 +339,8 @@ const startTimer = () => {
 };
 
 const confirmPayment = () => {
-  if (!buyerEmail.value) {
-    alert("Please enter your email.");
+  if (!form.value.buyerEmail || !form.value.name || !form.value.organization) {
+    alert("Please enter your details.");
     return;
   }
 
@@ -324,7 +348,6 @@ const confirmPayment = () => {
   showPaymentDialog.value = false;
   showSuccessDialog.value = true;
 
-  console.log("User email for document:", buyerEmail.value);
 };
 const getPdfPreview = (url) => {
   return url.replace(".pdf", ".jpg");
