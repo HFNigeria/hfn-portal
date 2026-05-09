e1a6f5ed736048e0df227e23bb440a59c55dca91
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-05-09 23:29:28 +0100

    Update SuperAdminEventsNewsletter.vue

--- src/views/SuperAdmin/SuperAdminEventsNewsletter.vue
+++ src/views/SuperAdmin/SuperAdminEventsNewsletter.vue
@@ -58,12 +58,14 @@ const editArticle = (article) => {
     audience: article.audience,
     is_featured: article.is_featured ?? false,
     featured_order: article.featured_order ?? 0,
-    // videos: article.videos ? [...article.videos] : [],
     videos: (article.videos || []).map((v) =>
       typeof v === "string" ? { media_type: "youtube", youtube_url: v } : v
     ),
     external_link: article.external_link || "",
     is_external: article.is_external ?? false,
+    publish_date: article.publish_date
+  ? article.publish_date.split("T")[0]
+  : "",
   };
 
   window.scrollTo({ top: 0, behavior: "smooth" });
@@ -174,6 +176,7 @@ const resetNewsForm = () => {
     videos: [],
     external_link: "",
     is_external: false,
+    publish_date: "",
   };
 };
 
@@ -197,6 +200,12 @@ const publishArticle = async (slug) => {
 const saveNews = async () => {
   try {
     const formData = new FormData();
+
+    if (!newsForm.value.publish_date) {
+  newsForm.value.publish_date = new Date()
+    .toISOString()
+    .split("T")[0];
+}
 
     Object.entries(newsForm.value).forEach(([key, value]) => {
       if (key === "featured_image") {
@@ -285,6 +294,7 @@ const newsForm = ref({
   videos: [],
   external_link: "",
   is_external: false,
+    publish_date: "",
 });
 
 const videoInput = ref("");
@@ -892,6 +902,21 @@ const closeSidebar = () => (showSidebar.value = false);
               class="input"
               placeholder="Excerpt"
             />
+            <div>
+  <label class="block mb-2 font-medium text-gray-700">
+    Publish Date
+  </label>
+
+  <input
+    type="date"
+    v-model="newsForm.publish_date"
+    class="input"
+  />
+
+  <p class="text-xs text-gray-500 mt-1">
+    Leave empty to use today's date
+  </p>
+</div>
 
             <div class="border rounded-lg p-4 bg-gray-50">
               <label class="flex items-center gap-3 mb-3">
